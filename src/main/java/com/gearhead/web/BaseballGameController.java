3432de75c053e70e29e5344f31129cfc44d3ae1f
gearhead89 <adsr2001@gmail.com>
2024-05-25 00:14:51 +0900

    homework #2

---
+++ src/main/java/com/gearhead/web/BaseballGameController.java
@@ -0,0 +1,16 @@
+package com.gearhead.web;
+
+import com.gearhead.BaseballGame;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseballGameController {
+    @GetMapping("/play")
+    public String play(){
+        BaseballGame bg = new BaseballGame();
+
+        return bg.getScore("123","321");
+    }
+}
